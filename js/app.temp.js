4649a16542b54e6be9a2a6dc2e2b8ee4f512c5d9
sejutisarkar <sejuti.dancinggirl8@gmail.com>
2017-09-29 15:40:37 +0530

    Update app.temp.js

--- js/app.temp.js
+++ js/app.temp.js
@@ -1,13 +1,39 @@
 class ProductList extends React.Component {
   state={
-      products:[],
+      products:[]
     };
-  //  this.handleProductUpVote = this.handleProductUpVote.bind(this);
-
   componentDidMount() {
 this.setState({
    products: Seed.products })
 }
+reverse =(event)=>{
+  event.preventDefault()
+  const reversed=this.state.products.sort((a,b)=>{
+  return (a.votes-b.votes)
+}
+)
+this.setState=({
+  products:reversed
+})
+}
+handleProductDownVote=(productId)=>{
+  console.log("reverse");
+  event.preventDefault()
+  const nextProducts = this.state.products.map((product)=>{
+  if(product.id === productId){
+    return Object.assign({},product,{
+      votes:product.votes-1
+    });
+  }
+  else{
+    return product
+  }}
+);
+this.setState({
+  products:nextProducts
+});
+}
+
 
   handleProductUpVote=(productId) => {
     console.log("voted");
@@ -40,6 +66,7 @@ votes={product.votes}
 submitterAvatarUrl={product.submitterAvatarUrl}
 productImageUrl={product.productImageUrl}
 handleProductUpVote={this.handleProductUpVote}
+handleProductDownVote={this.handleProductDownVote}
 />
 ));
     return (
@@ -54,6 +81,9 @@ class Product extends React.Component {
 handleUpVote=()=>{
   this.props.handleProductUpVote(this.props.id);
 }
+handleDownVote=()=>{
+  this.props.handleProductDownVote(this.props.id);
+}
 render() {
 return (
 <div className='item'>
@@ -66,6 +96,9 @@ return (
 <i className='large caret up icon' />
 </a>
 {this.props.votes}
+<a onClick={this.handleDownVote}>
+<i className='large caret down icon' />
+</a>
 </div>
 <div className='description'>
 <a href={this.props.url}>
